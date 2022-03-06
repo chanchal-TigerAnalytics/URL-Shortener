5dd83c5d29a8513adecb3877c59e57bb0b568a9c
Chanchal Kumar Maurya <chanchalkumarmaurya2299@gmail.com>
2022-03-07 00:09:36 +0530

    updated main.py according to the project requirements

--- main.py
+++ main.py
@@ -1,12 +1,32 @@
-from url_package import getShortURL, getLongURL
+from url_package import isShortURLPresent, getShortURL, getLongURL, showDatabase, updateLongURL
 
 if __name__ == "__main__":
-    url = 'https://www.codezax.com/'
-
-    # generates the shortURL from url
-    short = getShortURL(url, 'home')
-    print("Short URL: ", short)
-
-    # fetch the long url from short url   
+    fullURL = "https://www.fb.com/"
+    
+    #get the shortURL from fullURL
+    short = getShortURL(fullURL, 'fb')
+    print(short)
+    
+    # get the longURL from corresponding shortURL
     long = getLongURL(short)
     print(long)
+    
+    
+    #update the longURL
+    short = updateLongURL(short, "https://www.facebook.com/")
+    
+    #get the newLong URL
+    long = getLongURL(short)
+    print(long)
+    
+    # requests for database (username, password)
+    # only admin can see the database
+    showDatabase('chanchal', 'chanchal@123')
+    
+    
+    
+    
+    
+    
+    
+    
