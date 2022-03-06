1c848abc25fe39c89bf7066dab42b1ff1dcd7e86
Chanchal Kumar Maurya <chanchalkumarmaurya2299@gmail.com>
2022-03-06 22:58:27 +0530

    Created main.py file

---
+++ main.py
@@ -0,0 +1,12 @@
+from url_package import getShortURL, getLongURL
+
+if __name__ == "__main__":
+    url = 'https://www.codezax.com/'
+
+    # generates the shortURL from url
+    short = getShortURL(url)
+    print("Short URL: ", short)
+
+    # fetch the long url from short url   
+    long = getLongURL(short)
+    print(long)
